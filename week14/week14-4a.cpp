69d74d7c1e2209f7c2b63d5adff44b61c4fa7dab
asdfqwer13452 <afqwer123r@gmail.com>
2026-05-27 11:45:03 +0800

    week14

---
+++ week14/week14-4a.cpp
@@ -0,0 +1,9 @@
+//week14-4a.cpp SOIT108_ADVANCE_009B
+# include <stdio.h>
+int main()
+{
+    char s[10];
+    scanf("%s",s);
+    if(s[0]==s[3]&&s[1]==s[2]) printf("YES\n");
+    else printf("NO\n");
+}
